918e3606d2163711a129b6fb4aba9ff0ccac83fb
Ali Cem Kalsen <kalsendev@protonmail.com>
2026-09-02 16:35:17 +0300

    Add debug output

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,76 @@ glm::vec3 dirLightSpecular = glm::vec3(0.35f);
 
 float powerOfDirectional = 1.0f;
 
+
+GLenum glCheckError_(const char* file, int line)
+{
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR)
+	{
+		std::string error;
+
+		switch (errorCode)
+		{
+		case GL_INVALID_ENUM:                   error = "INVALID_ENUM"; break;
+		case GL_INVALID_VALUE:					error = "INVALID_VALUE"; break;
+		case GL_INVALID_OPERATION:              error = "INVALID_OPERATION"; break;
+		case GL_STACK_OVERFLOW:					error = "STACK_OVERFLOW"; break;
+		case GL_STACK_UNDERFLOW:                error = "STACK_UNDERFLOW"; break;
+		case GL_OUT_OF_MEMORY:                  error = "OUT_OF_MEMORY"; break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+		}
+
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+
+void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity,
+	GLsizei length, const char* message, const void* userParam)
+{
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:                 std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:       std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:     std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:         std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:         std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:               std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:                 std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:   std::cout << "Type: Deprecated Behavior"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:    std::cout << "Type: Undefined Behavior"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:           std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:           std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:                std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:            std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:             std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:                 std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:              std::cout << "Severity: High"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:            std::cout << "Severity: Medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:               std::cout << "Severity: Low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION:      std::cout << "Severity: Notification"; break;
+	} std::cout << std::endl;
+
+	std::cout << std::endl;
+}
+
+
 void resizeFramebufferAttachments(int width, int height)
 {
 	if (!height || !width)
@@ -399,7 +469,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "GraphicsPlayground", NULL, NULL);
 
@@ -422,6 +492,15 @@ int main()
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
+	}
+
+	int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags && GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 	}
 
 	IMGUI_CHECKVERSION();
@@ -638,7 +717,6 @@ int main()
 	unsigned int cubemapTexture = loadCubemap(faces);
 
 
-
     // SHADERS AND MODELS INITIALIZATION
 
 	Shader lightingShader("assets/shaders/lightingShader.vert", "assets/shaders/lightingShader.frag");
@@ -768,8 +846,6 @@ int main()
 		glDisable(GL_CULL_FACE);
 	
 		//dirLightDirection = glm::vec3(dirLightDirection.x, sin(glfwGetTime()) * 0.5f, cos(glfwGetTime()) * 0.5f);
-
-
 
 		glm::mat4 lightProjection = glm::ortho(-35.0f, 35.0f, -35.0f, 35.0f, near_plane, far_plane);
 		glm::mat4 lightView = glm::lookAt(lightPos,sceneCenter, glm::vec3(0.0, 1.0, 0.0));
